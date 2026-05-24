8601fc09d6e31d3381abe5ac506fd18b2c341945
hemanthbandaru2005-dotcom <hemanthbandaru2005@gmail.com>
2026-05-24 20:41:05 +0530

    Refactor addRow function and remove unused code

--- Script.js
+++ Script.js
@@ -1,71 +1,32 @@
-// SHOW SECTIONS
+function goToPage(page){
 
-function showSection(sectionId){
-
-    let sections = document.querySelectorAll('.section');
-
-    sections.forEach(section=>{
-        section.classList.remove('active');
-    });
-
-    document.getElementById(sectionId).classList.add('active');
+    window.location.href = page;
 }
 
-// SWITCH BETWEEN FIXED & NON-FIXED
+function addRow(){
 
-function showAssetTable(tableId){
-
-    let tables = document.querySelectorAll('.asset-table');
-
-    tables.forEach(table=>{
-        table.classList.remove('active-table');
-    });
-
-    document.getElementById(tableId).classList.add('active-table');
-}
-
-// ADD NEW ROWS
-
-function addRow(tableId){
-
-    let table = document.getElementById(tableId).getElementsByTagName('tbody')[0];
+    let table = document
+        .getElementById("assetTable")
+        .getElementsByTagName('tbody')[0];
 
     let rowCount = table.rows.length + 1;
 
     let row = table.insertRow();
 
-    if(tableId === "fixedTable"){
+    row.innerHTML = `
 
-        row.innerHTML = `
-            <td>${rowCount}</td>
-            <td contenteditable="true"></td>
-            <td contenteditable="true"></td>
-            <td contenteditable="true"></td>
-            <td><input type="date"></td>
-            <td contenteditable="true"></td>
-            <td><input type="date"></td>
-        `;
-    }
+        <td>${rowCount}</td>
 
-    else if(tableId === "nonFixedTable"){
+        <td contenteditable="true"></td>
 
-        row.innerHTML = `
-            <td>${rowCount}</td>
-            <td contenteditable="true"></td>
-            <td contenteditable="true"></td>
-            <td contenteditable="true"></td>
-            <td><input type="date"></td>
-        `;
-    }
+        <td contenteditable="true"></td>
 
-    else{
+        <td contenteditable="true"></td>
 
-        row.innerHTML = `
-            <td>${rowCount}</td>
-            <td contenteditable="true"></td>
-            <td contenteditable="true"></td>
-            <td contenteditable="true"></td>
-            <td><input type="date"></td>
-        `;
-    }
+        <td><input type="date"></td>
+
+        <td contenteditable="true"></td>
+
+        <td><input type="date"></td>
+    `;
 }
